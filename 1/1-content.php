cd76a95f5a958edd2738cba42c039c8a4d353a4d
NDH99 <hieunguyenduca5@gmail.com>
2019-11-02 08:19:45 +0700

    edit

--- 1/1-content.php
+++ 1/1-content.php
@@ -1,9 +1,17 @@
 <div class="type-1">
     <div class="container">
-    <div class="top">
-    <div class="contact">Contact us 24/7: +44 330 022 5700</div>
-    <div class="des">Express delivery and free returns within 28 days</div>
-    <div class="tandc">Terms & Conditions</div>
-    </div>
+        <div class="top">
+            <div class="row">
+                <div class="col-md-4 contact">
+                    Contact us 24/7: +44 330 022 5700
+                </div>
+                <div class="col-md-4 des">
+                    Express delivery and free returns within 28 days
+                </div>
+                <div class="col-md-4 tandc">
+                    Terms & Conditions
+                </div>
+            </div>
+        </div>
     </div>
 </div>
