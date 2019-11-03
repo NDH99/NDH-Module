723e14038762158a1d0e03b5bf22152d55427b50
NDH99 <hieunguyenduca5@gmail.com>
2019-11-03 22:49:03 +0700

    commit

--- 3/3-content.php
+++ 3/3-content.php
@@ -18,22 +18,83 @@ $url_path = str_replace('\\', '/', $url_path);
                 <div class="col-md-3"></div>
                 <div class="col-md-9">
                     <div class="row">
-                        <div class="col-md-4">
+                        <div class="col-xs-12 col-sm-6 col-md-4 pro">
                             <figure class="card card-product">
                                 <div class="img-wrap">
-                                    <img src="images/giay1.jpg" style="width:240px;height:292px;">
-                                    <img class="hover-img" src="images/giay2.jpg" style="width:240px;height:292px;">
+                                    <img src="images/giay1.jpg">
+                                    <img class="hover-img" src="images/giay2.jpg">
                                 </div>
                                 <figcaption class="info-wrap">
                                     <a href="#" class="tag">Accessories</a><br>
                                     <a href="#">Chic Scarf</a><br>
-                                    <span>$350.00</span>
+                                    <span class="price">$350.00</span>
                                 </figcaption>
                             </figure>
                         </div>
-                        <div class="col-md-4">
+                        <div class="col-xs-12 col-sm-6 col-md-4 pro">
+                            <figure class="card card-product">
+                                <div class="img-wrap">
+                                    <img src="images/giay1.jpg">
+                                    <img class="hover-img" src="images/giay2.jpg">
+                                </div>
+                                <figcaption class="info-wrap">
+                                    <a href="#" class="tag">Accessories</a><br>
+                                    <a href="#">Chic Scarf</a><br>
+                                    <span class="price">$350.00</span>
+                                </figcaption>
+                            </figure>
                         </div>
-                        <div class="col-md-4">
+                        <div class="col-xs-12 col-sm-6 col-md-4 pro">
+                            <figure class="card card-product">
+                                <div class="img-wrap">
+                                    <img src="images/giay1.jpg">
+                                    <img class="hover-img" src="images/giay2.jpg">
+                                </div>
+                                <figcaption class="info-wrap">
+                                    <a href="#" class="tag">Accessories</a><br>
+                                    <a href="#">Chic Scarf</a><br>
+                                    <span class="price">$350.00</span>
+                                </figcaption>
+                            </figure>
+                        </div>
+                        <div class="col-xs-12 col-sm-6 col-md-4 pro">
+                            <figure class="card card-product">
+                                <div class="img-wrap">
+                                    <img src="images/giay1.jpg">
+                                    <img class="hover-img" src="images/giay2.jpg">
+                                </div>
+                                <figcaption class="info-wrap">
+                                    <a href="#" class="tag">Accessories</a><br>
+                                    <a href="#">Chic Scarf</a><br>
+                                    <span class="price">$350.00</span>
+                                </figcaption>
+                            </figure>
+                        </div>
+                        <div class="col-xs-12 col-sm-6 col-md-4 pro">
+                            <figure class="card card-product">
+                                <div class="img-wrap">
+                                    <img src="images/giay1.jpg">
+                                    <img class="hover-img" src="images/giay2.jpg">
+                                </div>
+                                <figcaption class="info-wrap">
+                                    <a href="#" class="tag">Accessories</a><br>
+                                    <a href="#">Chic Scarf</a><br>
+                                    <span class="price">$350.00</span>
+                                </figcaption>
+                            </figure>
+                        </div>
+                        <div class="col-xs-12 col-sm-6 col-md-4 pro">
+                            <figure class="card card-product">
+                                <div class="img-wrap">
+                                    <img src="images/giay1.jpg">
+                                    <img class="hover-img" src="images/giay2.jpg">
+                                </div>
+                                <figcaption class="info-wrap">
+                                    <a href="#" class="tag">Accessories</a><br>
+                                    <a href="#">Chic Scarf</a><br>
+                                    <span class="price">$350.00</span>
+                                </figcaption>
+                            </figure>
                         </div>
                     </div>
                 </div>
