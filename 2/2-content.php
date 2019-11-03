05f84173c77ba36c6f5cd430ad938b8b0793b11b
NDH99 <hieunguyenduca5@gmail.com>
2019-11-03 22:18:42 +0700

    commit

--- 2/2-content.php
+++ 2/2-content.php
@@ -4,16 +4,18 @@
             <div class="row">
                 <div class="col-sm-12 col-md-6 img1">
                     <img src="images/h5-slide-img-1.jpg" alt="pic1" class="image img-responsive" style="width:100%">
-                    <h1 class="large">SHOP MEN</h1>
+                    
                     <div class="middle">
-                        <div class="text">SHOP MEN</div>
+                        <div class="large">SHOP MEN</div>
+                        <span class="text">SHOP MEN</span>
                     </div>
                 </div>
                 <div class="col-sm-12 col-md-6 img2">
                     <img src="images/h5-slide-img-2.jpg" alt="pic2" class="image img-responsive" style="width:100%">
-                    <h1 class="large">SHOP WOMEN</h1>
+                    
                     <div class="middle">
-                        <div class="text">SHOP WOMEN</div>
+                        <div class="large">SHOP WOMEN</div>
+                        <span class="text">SHOP WOMEN</span>
                     </div>
                 </div>
             </div>
