5a2c4f7f2322595730d89f6d43015c51716ed3a6
NDH99 <hieunguyenduca5@gmail.com>
2019-11-03 20:52:53 +0700

    commit

---
+++ 2/2-content.php
@@ -0,0 +1,196 @@
+<div class="type-1">
+    <div class="container">
+        <div class="top">
+            <div class="row">
+                <div class="col-md-4 contact">
+                    <a href="#">Contact us 24/7: +44 330 022 5700</a>
+                </div>
+                <div class="col-md-4 des">
+                    <a href="#">Express delivery and free returns within 28 days</a>
+                </div>
+                <div class="col-md-4 tandc">
+                    <a href="#">Terms & Conditions</a>
+                </div>
+            </div>
+        </div>
+        <div class="mid">
+            <div class="row">
+                <div class="col-md-4 english">
+                    <a href="#">English </a>
+                    <span>|</span>
+                    <a href="#"><i class="fa fa-user"></i></a>
+                </div>
+                <div class="col-md-4 logo">
+                    <a href="#"><img src="images/logo-img-1.png" alt="logo" class="img-responsive"></a>
+                </div>
+                <div class="col-md-4 find">
+                    <a href="#">
+                        <i class="fa fa-search"></i>
+                    </a>
+                    <a href="#">
+                        <i class="fa fa-shopping-bag"></i>
+                    </a>
+                    <a href="#">
+                        <i class="fa fa-heart"></i>
+                    </a>
+                </div>
+            </div>
+        </div>
+        <div class="navbar navbar-default navbar-static-top">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+            </div>
+            <div class="navbar-collapse collapse navbar-center">
+                <ul class="nav navbar-nav">
+                    <li class="dropdown menu-large dmenu">
+                        <a href=" #" class="dropdown-toggle" data-toggle="dropdown">WHAT'S NEW</a>
+                        <ul class="dropdown-menu megamenu row sm-menu">
+                            <li>
+                                <div class="col-sm-6 col-md-2">
+                                    <a href="#" class="thumbnail">
+                                    </a>
+                                </div>
+                                <div class="col-sm-6 col-md-2">
+                                    <a href="#" class="thumbnail">
+                                    </a>
+                                </div>
+                                <div class="col-sm-6 col-md-2">
+                                    <a href="#" class="thumbnail">
+                                    </a>
+                                </div>
+                            </li>
+                        </ul>
+                    </li>
+                    <li class="dropdown dmenu">
+                        <a href=" #" class="dropdown-toggle" data-toggle="dropdown">HOME</a>
+                        <ul class="dropdown-menu sm-menu">
+                            <li><a href="#">Category – Right sidebar</a></li>
+                            <li class="divider"></li>
+                            <li><a href="#">Category – Top slider 1</a></li>
+                            <li class="divider"></li>
+                            <li><a href="#">Category – Full width</a></li>
+                        </ul>
+                    </li>
+                    <li class="dropdown dmenu">
+                        <a href=" #" class="dropdown-toggle" data-toggle="dropdown">PAGES</a>
+                        <ul class="dropdown-menu sm-menu">
+                            <li><a href="#">Action</a></li>
+                            <li><a href="#">Category – Top slider 1</a></li>
+                            <li><a href="#">Category – Top slider 1</a></li>
+                            <li><a href="#">Category – Top slider 1</a></li>
+                            <li class="divider"></li>
+                            <li><a href="#">Category – Full width</a></li>
+                            <li><a href="#">Category – Top slider 1</a></li>
+                        </ul>
+                    </li>
+                    <li class="dropdown menu-large dmenu">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown">SHOP</a>
+                        <ul class="dropdown-menu megamenu row sm-menu">
+                            <li class="col-sm-3">
+                                <ul>
+                                    <li class="dropdown-header">Products Categoreis</li>
+                                    <li class="divider"></li>
+                                    <li><a href="#">Available glyphs</a></li>
+                                    <li class="divider"></li>
+                                    <li class="disabled"><a href="#">How to use</a></li>
+                                    <li class="divider"></li>
+                                    <li><a href="#">Examples</a></li>
+                                    <li class="divider"></li>
+                                </ul>
+                            </li>
+                            <li class="col-sm-3">
+                                <ul>
+                                </ul>
+                            </li>
+                            <li class="col-sm-3">
+                                <ul>
+                                    <li class="dropdown-header">Top Rated Products</li>
+                                    <li><a href="#">Keni Jeans Mog</a></li>
+                                    <li><a href="#">Mongoose L</a></li>
+                                </ul>
+                            </li>
+                            <li class="col-sm-3">
+                                <ul>
+                                    <li class="dropdown-header">Tags</li>
+                                    <li class="divider"></li>
+                                    <div class="tg">
+                                        <button>Beatifull</button>
+                                        <button>Best</button>
+                                        <button>Big</button>
+                                        <button>Blog</button>
+                                    </div>
+                                </ul>
+                            </li>
+                        </ul>
+
+                    </li>
+                    <li class="dropdown dmenu">
+                        <a href=" #" class="dropdown-toggle" data-toggle="dropdown">BLOG</a>
+                        <ul class="dropdown-menu sm-menu">
+                            <li><a href="#">Action</a></li>
+                            <li><a href="#">Category – Top slider 1</a></li>
+                            <li><a href="#">Category – Top slider 1</a></li>
+                            <li><a href="#">Category – Top slider 1</a></li>
+                            <li class="divider"></li>
+                            <li><a href="#">Category – Full width</a></li>
+                            <li><a href="#">Category – Top slider 1</a></li>
+                        </ul>
+                    </li>
+                    <li class="dropdown dmenu">
+                        <a href=" #" class="dropdown-toggle" data-toggle="dropdown">LOOKBOOK</a>
+                        <ul class="dropdown-menu sm-menu">
+                            <li><a href="#">Action</a></li>
+                            <li><a href="#">Category – Top slider 1</a></li>
+                            <li><a href="#">Category – Top slider 1</a></li>
+                            <li><a href="#">Category – Top slider 1</a></li>
+                            <li class="divider"></li>
+                            <li><a href="#">Category – Full width</a></li>
+                            <li><a href="#">Category – Top slider 1</a></li>
+                        </ul>
+                    </li>
+                    <li class="dropdown menu-large dmenu">
+                        <a href=" #" class="dropdown-toggle" data-toggle="dropdown">ELEMENTS</a>
+                        <ul class="dropdown-menu megamenu row sm-menu">
+                            <li class="col-sm-3">
+                                <ul>
+                                    <li class="dropdown-header">Featured</li>
+                                    <li><a href="#">Available glyphs</a></li>
+                                    <li><a href="#">How to use</a></li>
+                                    <li><a href="#">Examples</a></li>
+                                </ul>
+                            </li>
+                            <li class="col-sm-3">
+                                <ul>
+                                    <li class="dropdown-header">Infographic</li>
+                                    <li><a href="#">Available glyphs</a></li>
+                                    <li><a href="#">How to use</a></li>
+                                    <li><a href="#">Examples</a></li>
+                                </ul>
+                            </li>
+                            <li class="col-sm-3">
+                                <ul>
+                                    <li class="dropdown-header">Classic</li>
+                                    <li><a href="#">Available glyphs</a></li>
+                                    <li><a href="#">How to use</a></li>
+                                    <li><a href="#">Examples</a></li>
+                                </ul>
+                            </li>
+                            <li class="col-sm-3">
+                                <ul>
+                                    <li class="dropdown-header">Typography</li>
+                                    <li><a href="#">Available glyphs</a></li>
+                                    <li><a href="#">How to use</a></li>
+                                    <li><a href="#">Examples</a></li>
+                                </ul>
+                            </li>
+                        </ul>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</div>
