ce957e43b7fc300eff06af34ccefa30ef7a36237
NDH99 <hieunguyenduca5@gmail.com>
2019-11-03 21:04:33 +0700

    commit

--- 2/2-content.php
+++ 2/2-content.php
@@ -1,10 +1,18 @@
-<div class="type-1">
+<div class="type-2">
     <div class="container">
         <div class="products">
             <div class="row">
-                <div class="col-md-6 img1">
+                <div class="col-sm-12 col-md-6 img1">
+                    <img src="images/h5-slide-img-1.jpg" class="img-responsive" alt="pic1">
+                    <div class="text-center">
+                        <button type="button" class="btn btn-primary">SHOP MEN</button>
+                    </div>
                 </div>
-                <div class="col-md-6 img2">
+                <div class="col-sm-12 col-md-6 img2">
+                    <img src="images/h5-slide-img-2.jpg" class="img-responsive" alt="pic2">
+                    <div class="text-center">
+                        <button type="button" class="btn btn-primary">SHOP WOMEN</button>
+                    </div>
                 </div>
             </div>
         </div>
