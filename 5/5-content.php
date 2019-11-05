463dce1824173b7ae205d59daa4783c8ee54a600
NDH99 <hieunguyenduca5@gmail.com>
2019-11-05 13:45:28 +0700

    commit

--- 5/5-content.php
+++ 5/5-content.php
@@ -1,5 +1,37 @@
 <div class="type-5">
-	<div class="container">
-	
-	</div>
+    <div class="container">
+
+        <div class="sub">
+            <h6>SUBSCRIBE TO NEWSLETTER</h6>
+        </div>
+        <div class="email">
+            <input type="email" name="your-email" value="" size="40" placeholder="E-mail address">
+        </div>
+        <div class="send">
+            <button type="submit">Send</button>
+        </div>
+
+        <div class="menu-footer">
+            <ul class="menu">
+                <li><a href="#">What’s New</a></li>
+				<li><a href="#">Women</a></li>
+                <li><a href="#">Men</a></li>
+                <li><a href="#">Kids</a></li>
+                <li><a href="#">Trend</a></li>
+                <li><a href="#">Clothes</a></li>
+                <li><a href="#">Shoes</a></li>
+                <li><a href="#">Dresses</a></li>
+            </ul>
+        </div>
+
+		<div class="menu-icon">
+            <ul class="icon">
+				<li><a href="#"></a></li>
+                <li><a href="#"></a></li>
+                <li><a href="#"></a></li>
+                <li><a href="#"></a></li>
+                <li><a href="#"></a></li>
+            </ul>
+        </div>
+    </div>
 </div>
