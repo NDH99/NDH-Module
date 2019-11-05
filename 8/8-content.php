8da9636ebcecee5654606d10c88fbd393bd312dc
NDH99 <hieunguyenduca5@gmail.com>
2019-11-05 16:38:59 +0700

    commit

---
+++ 8/8-content.php
@@ -0,0 +1,15 @@
+<div class="type-8">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4">
+                <img src="images/product-category-img-1.jpg" alt="women">
+            </div>
+            <div class="col-md-4">
+                <img src="images/product-category-img-2.jpg" alt="men">
+            </div>
+            <div class="col-md-4">
+                <img src="images/product-category-img-3.jpg" alt="kid">
+            </div>
+        </div>
+    </div>
+</div>
