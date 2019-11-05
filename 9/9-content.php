a40920cadebf104cfd227904d66a8818e9ecc102
NDH99 <hieunguyenduca5@gmail.com>
2019-11-05 16:57:44 +0700

    commit

--- 9/9-content.php
+++ 9/9-content.php
@@ -1,5 +1,37 @@
 <div class="type-9">
-    <div class="container">
-        
+    <div class="swiper-container">
+        <div class="swiper-wrapper">
+            <div class="swiper-slide">
+                <figure class="card card-product">
+                    <div class="img-wrap">
+                        <img src="images/h1-slide-img-2.jpg">
+                        <div class="middle">
+                            <span class="text">SALE</span>
+                        </div>
+                    </div>
+                </figure>
+            </div>
+            <div class="swiper-slide">
+                <figure class="card card-product">
+                    <div class="img-wrap">
+                        <img src="images/h1-slide-img-4.jpg">
+                    </div>
+                </figure>
+            </div>
+            <div class="swiper-slide">
+                <figure class="card card-product">
+                    <div class="img-wrap">
+                        <img src="images/slide-kids.jpg">
+                    </div>
+                </figure>
+            </div>
+        </div>
+        <!-- Add Pagination -->
+        <div class="swiper-pagination"></div>
+        <!-- Add Arrows -->
+        <br>
+        <br>
+        <div class="swiper-button-next swiper-button-black" style="top: 147px; right:0"></div>
+        <div class="swiper-button-prev swiper-button-black" style="top: 147px; left:0;"></div>
     </div>
 </div>
