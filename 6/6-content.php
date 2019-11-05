5c380f628bf19b021c23c8f6aee3059e350bfdfb
NDH99 <hieunguyenduca5@gmail.com>
2019-11-05 16:01:51 +0700

    Update 6-content.php

--- 6/6-content.php
+++ 6/6-content.php
@@ -5,12 +5,36 @@
 
         <!-- Modal -->
         <div id="myModal" class="modal fade" role="dialog">
-            <div class="modal-dialog">
-
+            <div class="modal-dialog modal-lg" role="document">
                 <!-- Modal content-->
                 <div class="modal-content">
                     <div class="modal-header">
                         <button type="button" class="close" data-dismiss="modal">&times;</button>
+                        <div class="row">
+                            <div class="col-md-6 img-pop">
+                                <img src="images/pop-up.jpg" alt="pop" class="img-responsive">
+                            </div>
+                            <div class="col-md-6 right">
+                                <h2>Be the first to know!</h2>
+                                <div class="subtitle">
+                                    Sign up for emails to get our latest style news before everybody else.
+                                </div>
+                                <div class="sub">
+                                    <h5>SUBSCRIBE TO NEWSLETTER</h5>
+                                </div>
+                                <div class="email">
+                                    <input type="email" name="your-email" value="" size="40"
+                                        placeholder="E-mail address">
+                                </div>
+                                <div class="send">
+                                    <button type="submit">Send</button>
+                                </div>
+
+                                <div class="checkbox">
+                                    <label><input type="checkbox" value="">Prevent This Pop-up</label>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
