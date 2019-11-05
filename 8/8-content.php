e455100c19c6321114e6adc1a33c3fbfea642124
NDH99 <hieunguyenduca5@gmail.com>
2019-11-05 16:40:10 +0700

    commit

--- 8/8-content.php
+++ 8/8-content.php
@@ -1,13 +1,13 @@
 <div class="type-8">
     <div class="container">
-        <div class="row">
-            <div class="col-md-4">
+        <div class="row pro">
+            <div class="col-md-4 women">
                 <img src="images/product-category-img-1.jpg" alt="women">
             </div>
-            <div class="col-md-4">
+            <div class="col-md-4 men">
                 <img src="images/product-category-img-2.jpg" alt="men">
             </div>
-            <div class="col-md-4">
+            <div class="col-md-4 kid">
                 <img src="images/product-category-img-3.jpg" alt="kid">
             </div>
         </div>
