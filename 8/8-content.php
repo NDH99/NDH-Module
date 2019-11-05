f3620d898378df3bf06f7f0d5e4d25d52e3bcc8d
NDH99 <hieunguyenduca5@gmail.com>
2019-11-05 16:46:07 +0700

    commit

--- 8/8-content.php
+++ 8/8-content.php
@@ -1,14 +1,17 @@
 <div class="type-8">
     <div class="container">
         <div class="row pro">
-            <div class="col-md-4 women">
-                <img src="images/product-category-img-1.jpg" alt="women">
+            <div class="col-xs-12 col-sm-6 col-md-4 women">
+                <img src="images/product-category-img-1.jpg" alt="women" class="img-responsive">
+                <p>WOMEN</p>
             </div>
-            <div class="col-md-4 men">
-                <img src="images/product-category-img-2.jpg" alt="men">
+            <div class="col-xs-12 col-sm-6 col-md-4 men">
+                <img src="images/product-category-img-2.jpg" alt="men" class="img-responsive">
+                <p>MEN</p>
             </div>
-            <div class="col-md-4 kid">
-                <img src="images/product-category-img-3.jpg" alt="kid">
+            <div class="col-xs-12 col-sm-6 col-md-4 kid">
+                <img src="images/product-category-img-3.jpg" alt="kid" class="img-responsive">
+                <p>KID</p>
             </div>
         </div>
     </div>
