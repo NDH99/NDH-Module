5c4fbc963e470507f4d51609b25f15b5d97e4213
NDH99 <hieunguyenduca5@gmail.com>
2019-11-05 15:13:27 +0700

    commit

--- 6/6-content.php
+++ 6/6-content.php
@@ -1,4 +1,19 @@
 <div class="type-6">
     <div class="container">
+        <!-- Trigger the modal with a button -->
+        <button type="button" class="btn btn-info btn-lg" data-toggle="modal" data-target="#myModal">Open Modal</button>
+
+        <!-- Modal -->
+        <div id="myModal" class="modal fade" role="dialog">
+            <div class="modal-dialog">
+
+                <!-- Modal content-->
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <button type="button" class="close" data-dismiss="modal">&times;</button>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </div>
