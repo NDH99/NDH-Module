99c19755fa7e7e157ad788e5462c3e83a175825d
NDH99 <hieunguyenduca5@gmail.com>
2019-11-06 07:37:02 +0700

    Update 11-content.php

--- 11/11-content.php
+++ 11/11-content.php
@@ -1,5 +1,22 @@
 <div class="type-11">
-	<div class="container">
-					
-	</div>
+    <div class="container">
+        <div class="swiper-container">
+            <div class="swiper-wrapper">
+                <div class="swiper-slide">
+                    <figure class="card card-product">
+                        <div class="img-wrap">
+                            <img src="images/product-img-1.jpg">
+                        </div>
+                    </figure>
+                </div>
+            </div>
+            <!-- Add Pagination -->
+            <div class="swiper-pagination"></div>
+            <!-- Add Arrows -->
+            <br>
+            <br>
+            <div class="swiper-button-next swiper-button-black" style="top: 147px; right:0"></div>
+            <div class="swiper-button-prev swiper-button-black" style="top: 147px; left:0;"></div>
+        </div>
+    </div>
 </div>
