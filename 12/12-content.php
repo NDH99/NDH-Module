fb61b0f3369193bc1b74f36293700fdb1bd514f8
NDH99 <hieunguyenduca5@gmail.com>
2019-11-06 08:48:55 +0700

    commit

---
+++ 12/12-content.php
@@ -0,0 +1,29 @@
+<div class="type-12">
+    <div class="container">
+        <div class="comment-title">
+            <h6>4 COMMENT</h6>
+        </div>
+        <div class="comment">
+            <ul>
+                <li>
+                    <div class="image">
+                        <img src="images/User-2-100x100.jpg" alt="user1" class="img-responsive">
+                    </div>
+                    <div class="text">
+                        <div class="name">Ed George</div>
+                        <div class="date">November 15, 2018</div>
+                        <div class="action">
+                            <a href="#">reply</a>
+                        </div>
+                        <div class="text-holder">Awesome and one of my favorites this season!</div>
+                    </div>
+                </li>
+            </ul>
+
+
+        </div>
+        <div class="leave-comment"></div>
+        <div class="infor"></div>
+        <div class="submit"></div>
+    </div>
+</div>
