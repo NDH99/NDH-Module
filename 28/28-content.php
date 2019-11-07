79f28bb6c6aea205bf9198acaf98abd361d6d07d
NDH99 <hieunguyenduca5@gmail.com>
2019-11-07 09:13:50 +0700

    commit

---
+++ 28/28-content.php
@@ -0,0 +1,32 @@
+<div class="type-28">
+    <div class="container">
+        <div class="text">
+            <h3>Back in stock</h3>
+            <h1>OUR BEST SWIM</h1>
+        </div>
+        <div class="text">
+            <h3>Back in stock</h3>
+            <h1>OUR BEST SWIM</h1>
+        </div>
+        <div class="text">
+            <h3>Back in stock</h3>
+            <h1>OUR BEST SWIM</h1>
+        </div>
+        <div class="text">
+            <h3>Back in stock</h3>
+            <h1>OUR BEST SWIM</h1>
+        </div>
+        <div class="text">
+            <h3>Back in stock</h3>
+            <h1>OUR BEST SWIM</h1>
+        </div>
+        <div class="text">
+            <h3>Back in stock</h3>
+            <h1>OUR BEST SWIM</h1>
+        </div>
+        <div class="text">
+            <h3>Back in stock</h3>
+            <h1>OUR BEST SWIM</h1>
+        </div>
+    </div>
+</div>
