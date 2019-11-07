361e21ab6ae5347ceb41009943667dd0f314dd20
NDH99 <hieunguyenduca5@gmail.com>
2019-11-07 13:30:09 +0700

    commit

---
+++ 34/34-content.php
@@ -0,0 +1,49 @@
+<div class="type-34">
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12 col-sm-6 col-md-4">
+                <a href="#">
+                    <img src="./images/1.jpg" alt="" class="img-responsive img-holder">
+                </a>
+                <h4>FLORAL RHAPSODY</h4>
+                <span>Boho</span>
+            </div>
+            <div class="col-xs-12 col-sm-6 col-md-4">
+                <a href="#">
+                    <img src="./images/2.jpg" alt="" class="img-responsive img-holder">
+                </a>
+                <h4>FESTIVE FEELING</h4>
+                <span>Boho</span>
+            </div>
+            <div class="col-xs-12 col-sm-6 col-md-4">
+                <a href="#">
+                    <img src="./images/3.jpg" alt="" class="img-responsive img-holder">
+                </a>
+                <h4>SUMMERTIME JOY</h4>
+                <span>Boho</span>
+            </div>
+            <div class="col-xs-12 col-sm-6 col-md-4">
+                <a href="#">
+                    <img src="./images/4.jpg" alt="" class="img-responsive img-holder">
+                </a>
+                <h4>FLORAL RHAPSODY</h4>
+                <span>Boho</span>
+            </div>
+            <div class="col-xs-12 col-sm-6 col-md-4">
+                <a href="#">
+                    <img src="./images/5.jpg" alt="" class="img-responsive img-holder">
+                </a>
+                <h4>FESTIVE FEELING</h4>
+                <span>Boho</span>
+            </div>
+            <div class="col-xs-12 col-sm-6 col-md-4">
+                <a href="#">
+                    <img src="./images/6.jpg" alt="" class="img-responsive img-holder">
+                </a>
+                <h4>SUMMERTIME JOY</h4>
+                <span>Boho</span>
+            </div>
+
+        </div>
+    </div>
+</div>
