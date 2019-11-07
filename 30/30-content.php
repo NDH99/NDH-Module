19bfd9fed852ae65eb3c589bde50a4a78acb993b
NDH99 <hieunguyenduca5@gmail.com>
2019-11-07 08:20:49 +0700

    commit

---
+++ 30/30-content.php
@@ -0,0 +1,58 @@
+<div class="type-30">
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12 col-sm-6 col-md-4">
+                <img src="./images/1.jpg" alt="" class="img-responsive">
+                <div class="title">
+                    <h2>SHARON BURKE</h2>
+                    <span>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque sed
+                        laudantium, totam rem aperiam, eaque ipsa ab</span>
+                    <ul class="sosical">
+                        <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+                        <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+                        <li><a href="#"><i class="fa fa-pinterest"></i></a></li>
+                    </ul>
+                </div>
+            </div>
+            <div class="col-xs-12 col-sm-6 col-md-4">
+                <img src="./images/2.jpg" alt="" class="img-responsive">
+                <div class="title">
+                    <h2>ERIC NEWMAN</h2>
+                    <span>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque sed
+                        laudantium, totam rem aperiam, eaque ipsa ab</span>
+                    <ul class="sosical">
+                        <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+                        <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+                        <li><a href="#"><i class="fa fa-pinterest"></i></a></li>
+                    </ul>
+                </div>
+            </div>
+            <div class="col-xs-12 col-sm-6 col-md-4">
+                <img src="./images/3.jpg" alt="" class="img-responsive">
+                <div class="title">
+                    <h2>ANGELA MARSHALL</h2>
+                    <span>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque sed
+                        laudantium, totam rem aperiam, eaque ipsa ab</span>
+                    <ul class="sosical">
+                        <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+                        <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+                        <li><a href="#"><i class="fa fa-pinterest"></i></a></li>
+                    </ul>
+                </div>
+            </div>
+            <div class="col-xs-12 col-sm-6 col-md-4">
+                <img src="./images/4.jpg" alt="" class="img-responsive">
+                <div class="title">
+                    <h2>SHARON WEAVER</h2>
+                    <span>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque sed
+                        laudantium, totam rem aperiam, eaque ipsa ab</span>
+                    <ul class="sosical">
+                        <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+                        <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+                        <li><a href="#"><i class="fa fa-pinterest"></i></a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
