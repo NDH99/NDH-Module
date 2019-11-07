a86031164acedb5d4af3c20528c29c42a9c974f6
NDH99 <hieunguyenduca5@gmail.com>
2019-11-07 10:21:09 +0700

    commit

---
+++ 35/35-content.php
@@ -0,0 +1,148 @@
+<div class="type-35">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-3">
+                <div class="item-left">
+                    <h3>CUSTOMER SERVICE</h3>
+                    <p>“Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+                        labore
+                        et dolore magna qua tellus erat mattis luctus.”</p>
+                    <button class="btn btn-phone">+88 (0) 01001110</button>
+                    <p>Call us, we are open 24 hours a day, 7
+                        days a week.</p>
+                    <p>We ship almost everywhere
+                        Free shipping on returns & exchanges
+                        within the continental US</p>
+                </div>
+
+            </div>
+            <div class="col-md-9">
+                <div class="item-right">
+                    <div class="panel-group" id="accordion" role="tablist" aria-multiselectable="true">
+                        <div class="panel panel-default">
+                            <div class="panel-heading" role="tab" id="headingOne">
+                                <h4 class="panel-title">
+                                    <a role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseOne"
+                                        aria-expanded="true" aria-controls="collapseOne" class='collapsed'>
+                                        WHAT IS YOUR RETURN POLICY?
+                                    </a>
+                                </h4>
+                            </div>
+                            <div id="collapseOne" class="panel-collapse collapse" role="tabpanel"
+                                aria-labelledby="headingOne">
+                                <div class="panel-body">
+                                    Curabitur commodo, metus nec aliquam cursus, lorem enim laoreet sapien sed ut
+                                    perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque
+                                    laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi
+                                    architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam Nemo enim ipsam
+                                    voluptatem quia sit aspernatur aut odit aut voluptas.
+
+                                    Sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt, meque
+                                    porro quisquam est dolorem ipsum quia dolor consectetu varius ac tempor tincidunt in
+                                    magna hendrerit tempus.
+                                </div>
+                            </div>
+                        </div>
+                        <div class="panel panel-default">
+                            <div class="panel-heading" role="tab" id="headingTwo">
+                                <h4 class="panel-title">
+                                    <a class="collapsed" role="button" data-toggle="collapse" data-parent="#accordion"
+                                        href="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                                        HOW DO I RETURN OR EXCHANGE MY ORDER?
+                                    </a>
+                                </h4>
+                            </div>
+                            <div id="collapseTwo" class="panel-collapse collapse" role="tabpanel"
+                                aria-labelledby="headingTwo">
+                                <div class="panel-body">
+                                    Curabitur commodo, metus nec aliquam cursus, lorem enim laoreet sapien sed ut
+                                    perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque
+                                    laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi
+                                    architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam Nemo enim ipsam
+                                    voluptatem quia sit aspernatur aut odit aut voluptas.
+
+                                    Sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt, meque
+                                    porro quisquam est dolorem ipsum quia dolor consectetu varius ac tempor tincidunt in
+                                    magna hendrerit tempus.
+                                </div>
+                            </div>
+                        </div>
+                        <div class="panel panel-default">
+                            <div class="panel-heading" role="tab" id="headingThree">
+                                <h4 class="panel-title">
+                                    <a class="collapsed" role="button" data-toggle="collapse" data-parent="#accordion"
+                                        href="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+                                        CAN I CHANGE MY ORDER?
+                                    </a>
+                                </h4>
+                            </div>
+                            <div id="collapseThree" class="panel-collapse collapse" role="tabpanel"
+                                aria-labelledby="headingThree">
+                                <div class="panel-body">
+                                    Curabitur commodo, metus nec aliquam cursus, lorem enim laoreet sapien sed ut
+                                    perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque
+                                    laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi
+                                    architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam Nemo enim ipsam
+                                    voluptatem quia sit aspernatur aut odit aut voluptas.
+
+                                    Sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt, meque
+                                    porro quisquam est dolorem ipsum quia dolor consectetu varius ac tempor tincidunt in
+                                    magna hendrerit tempus.
+                                </div>
+                            </div>
+                        </div>
+                        <div class="panel panel-default">
+                            <div class="panel-heading" role="tab" id="headingFour">
+                                <h4 class="panel-title">
+                                    <a class="collapsed" role="button" data-toggle="collapse" data-parent="#accordion"
+                                        href="#collapseFour" aria-expanded="false" aria-controls="collapseFour">
+                                        DO YOU SHIP INTERNATIONALLY?
+                                    </a>
+                                </h4>
+                            </div>
+                            <div id="collapseFour" class="panel-collapse collapse" role="tabpanel"
+                                aria-labelledby="headingFour">
+                                <div class="panel-body">
+                                    Curabitur commodo, metus nec aliquam cursus, lorem enim laoreet sapien sed ut
+                                    perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque
+                                    laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi
+                                    architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam Nemo enim ipsam
+                                    voluptatem quia sit aspernatur aut odit aut voluptas.
+
+                                    Sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt, meque
+                                    porro quisquam est dolorem ipsum quia dolor consectetu varius ac tempor tincidunt in
+                                    magna hendrerit tempus.
+                                </div>
+                            </div>
+                        </div>
+                        <div class="panel panel-default">
+                            <div class="panel-heading" role="tab" id="headingFive">
+                                <h4 class="panel-title">
+                                    <a class="collapsed" role="button" data-toggle="collapse" data-parent="#accordion"
+                                        href="#collapseFive" aria-expanded="false" aria-controls="collapseFive">
+                                        WHAT PAYMENT METHODS DO YOU ACCEPT?
+                                    </a>
+                                </h4>
+                            </div>
+                            <div id="collapseFive" class="panel-collapse collapse" role="tabpanel"
+                                aria-labelledby="headingFive">
+                                <div class="panel-body">
+                                    Curabitur commodo, metus nec aliquam cursus, lorem enim laoreet sapien sed ut
+                                    perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque
+                                    laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi
+                                    architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam Nemo enim ipsam
+                                    voluptatem quia sit aspernatur aut odit aut voluptas.
+
+                                    Sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt, meque
+                                    porro quisquam est dolorem ipsum quia dolor consectetu varius ac tempor tincidunt in
+                                    magna hendrerit tempus.
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
