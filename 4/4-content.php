e6e6029278c59712896d47a15c044fd3514a9eac
NDH99 <hieunguyenduca5@gmail.com>
2019-11-07 15:08:14 +0700

    commit

---
+++ 4/4-content.php
@@ -0,0 +1,23 @@
+<div class="type-4">
+    <h2>EDITORS'_PICKS</h2>
+    <div class="row">
+        <div class="col-md-2">
+            <img src="./images/1.jpg" alt="" class="img-responsive">
+        </div>
+        <div class="col-md-2">
+            <img src="./images/2.jpg" alt="" class="img-responsive">
+        </div>
+        <div class="col-md-2">
+            <img src="./images/3.jpg" alt="" class="img-responsive">
+        </div>
+        <div class="col-md-2">
+            <img src="./images/4.jpg" alt="" class="img-responsive">
+        </div>
+        <div class="col-md-2">
+            <img src="./images/5.jpg" alt="" class="img-responsive">
+        </div>
+        <div class="col-md-2">
+            <img src="./images/6.jpg" alt="" class="img-responsive">
+        </div>
+    </div>
+</div>
