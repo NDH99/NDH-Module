94cc464f9e5a9dd6e981186ec13cfcae04c0da6e
NDH99 <hieunguyenduca5@gmail.com>
2019-11-07 14:13:38 +0700

    commit

---
+++ 33/33-content.php
@@ -0,0 +1,65 @@
+<div class="type-33">
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12 col-sm-6 col-md-3">
+                <span>CUSTOMER SERVICE</span>
+                <ul class="list">
+                    <li><a href="#">+ 1 855 635 9272</a></li>
+                    <li><a href="#">Monday to Friday: 8.30 am – 7.30 pm</a></li>
+                    <li><a href="#">Saturday: 10:00 am – 4:00 pm</a></li>
+                    <li><a href="#">gioia@elated-themes.com</a></li>
+                </ul>
+            </div>
+            <div class="col-xs-12 col-sm-6 col-md-3">
+                <span>ONLINE ORDERS</span>
+                <ul class="list">
+                    <li><a href="#">+ 1 855 635 9272</a></li>
+                    <li><a href="#">Monday to Friday: 8.30 am – 7.30 pm</a></li>
+                    <li><a href="#">Saturday: 10:00 am – 4:00 pm</a></li>
+                    <li><a href="#">gioia@elated-themes.com</a></li>
+                </ul>
+            </div>
+            <div class="col-xs-12 col-sm-6 col-md-3">
+                <span>MARKETING TEAM</span>
+                <ul class="list">
+                    <li><a href="#">+ 1 855 635 9272</a></li>
+                    <li><a href="#">Monday to Friday: 8.30 am – 7.30 pm</a></li>
+                    <li><a href="#">Saturday: 10:00 am – 4:00 pm</a></li>
+                    <li><a href="#">gioia@elated-themes.com</a></li>
+                </ul>
+            </div>
+            <div class="col-xs-12 col-sm-6 col-md-3">
+                <span>SOCIAL MEDIA</span>
+                <ul class="list">
+                    <li><a href="#">facebook: Gioia Fashion</a></li>
+                    <li><a href="#">instagram: #gioiafashion</a></li>
+                    <li><a href="#">twitter: @gioia-fashion</a></li>
+                    <li><a href="#">pinterest: Gioia Fashion</a></li>
+                    <li><a href="#">linkedin: gioia</a></li>
+                </ul>
+            </div>
+        </div>
+        <div class="product">
+            <div class="row">
+                <div class="col-md-6">
+                    <img src="./images/paris.jpg" alt="" class="img-responsive">
+                    <p>PARIES</p>
+                    <div class="depsiton">
+                        <span>3rd floor, 10 East 52nd Street, New York, NYC, 10022, United States </span>
+                        <button class="btn btn-get">GET DIRECTIONS</button>
+                    </div>
+                </div>
+                <div class="col-md-6">
+                    <img src="./images/berlin.jpg" alt="" class="img-responsive">
+                    <p>BERLIN</p>
+                    <div class="depsiton">
+                        <span>3rd floor, 10 East 52nd Street, New York, NYC, 10022, United States</span>
+                        <button class="btn btn-get">GET DIRECTIONS</button>
+                    </div>
+                    
+                </div>
+            </div>
+            
+        </div>
+    </div>
+</div>
