f9d7f862d11fe7cda621f6ecbca04922120b50b5
NDH99 <hieunguyenduca5@gmail.com>
2019-11-07 11:41:35 +0700

    commit

---
+++ 32/32-content.php
@@ -0,0 +1,41 @@
+<div class="type-32">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4">
+                <h2>PARIS</h2>
+                <div class="list">
+                    <a href="#">Head Office</a>
+                    <a href="#">5 avenue Percier</a>
+                    <a href="#">Paris</a>
+                    <a href="#">France</a>
+                    <a href="#">+33 1 44 94 33 33</a>
+                </div>
+                <h2>NEW YORK</h2>
+                <div class="list">
+                    <a href="#">3rd floor</a>
+                    <a href="#">10 East 52nd Street</a>
+                    <a href="#">New York, NYC</a>
+                    <a href="#">United States</a>
+                    <a href="#">+1 212 957 11 00</a>
+                </div>
+            </div>
+            <div class="col-md-8">
+                <div class="contact">
+                    <h2>SEND US A MESSAGE</h2>
+                    <div class="form-group">
+                            <textarea name="txtMsg" class="form-control" placeholder="Type Your Message *" style="width: 100%; height: 150px;"></textarea>
+                        </div>
+                        <div class="form-group">
+                            <input type="text" name="txtName" class="form-control fr-name" placeholder="Your Name here *" value="" />
+                        </div>
+                        <div class="form-group">
+                            <input type="text" name="txtEmail" class="form-control fr-email" placeholder="E-mail addres *" value="" />
+                        </div>
+                        <div class="form-group">
+                            <input type="submit" name="btnSubmit" class="btnContact" value="Send" />
+                        </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
