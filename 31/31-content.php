31b3c04ad9b9d9602eef76337d9d1ba9175f79f4
NDH99 <hieunguyenduca5@gmail.com>
2019-11-07 10:36:10 +0700

    commit

---
+++ 31/31-content.php
@@ -0,0 +1,42 @@
+<div class="type-31">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6">
+                <img src="./images/p2.jpg" alt="" class="img-responsive">
+            </div>
+            <div class="col-md-1"></div>
+            <div class="col-md-4">
+                <div class="title">
+                    <span>Founder</span>
+                    <h1>MARILYN BAKER</h1>
+                </div>
+                <div class="depsition">
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. A doloremque itaque quidem expedita
+                        laudantium mollitia, libero voluptate, placeat officia minus fugiat cupiditate. Voluptates,
+                        velit
+                        facere eveniet voluptatum nesciunt magni quibusdam.</p>
+                    <p>“Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque
+                        laudantium,
+                        totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae
+                        dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut
+                        fugit.
+                    </p>
+                </div>
+                <div class="share">
+                    <div class="twitter">
+                        <span>TWITTER</span><a href="#">https://twitter.com/</a>
+                    </div>
+                    <div class="instagram">
+                        <span>INSTAGRAM</span><a href="#">https://instagram.com/</a>
+                    </div>
+                    <div class="fb">
+                        <span>FACEBOOK</span><a href="#">https://facebook.com</a>
+                    </div>
+                </div>
+                <img src="./images/p1.png" alt="" class="img-responsive">
+            </div>
+            <div class="col-md-1"></div>
+        </div>
+    </div>
+
+</div>
