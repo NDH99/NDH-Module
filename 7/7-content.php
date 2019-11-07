7474655023c4ffd75aa1955ec3d921d6d17a559c
NDH99 <hieunguyenduca5@gmail.com>
2019-11-07 14:29:02 +0700

    commit

---
+++ 7/7-content.php
@@ -0,0 +1,88 @@
+<div class="type-7">
+    <div class="container">
+        <div class="swiper-container">
+            <div class="swiper-wrapper">
+                <div class="swiper-slide">
+                    <img src="./images/1.jpg" alt="">
+                </div>
+                <div class="swiper-slide">
+                    <img src="./images/2.jpg" alt="">
+                </div>
+                <div class="swiper-slide">
+                    <img src="./images/3.jpg" alt="">
+                </div>
+            </div>
+            <!-- The pagination -->
+            <div class="swiper-pagination"></div>
+            <!-- Add Arrows -->
+            <div class="swiper-button-next" style="color: #000000;"></div>
+            <div class="swiper-button-prev" style="color: #000000;"></div>
+        </div>
+        <div class="row" style="margin-bottom: 30px;">
+            <div class="col-md-8">
+                <img src="./images/4.jpg" alt="" class="img-responsive" style="height: 425px;">
+                <div class="text">
+                    <a href="#">Straw Hat $45.00</a>
+                    <a href="#">Cashmere Scarf $70.00</a>
+                    <a href="#">Cotton Tunic $115.00</a>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <img src="./images/5.jpg" alt="" class="img-responsive">
+                <div class="text">
+                    <a href="#">Silk Scarf $45.00</a>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-5th-1 col-sm-4 col-md-offset-0 col-sm-offset-2">
+                <img src="./images/11.jpg" alt="" class="img-responsive">
+                <div class="depsition">
+                    <a href="#">Clothes, Dresses, Women, WOMEN</a>
+                    <a href="#"><p>Pastel Dress</p></a>
+                    <span class="price">$215.00</span>
+                </div>
+            </div>
+            <div class="col-md-5th-1 col-sm-4">
+                <img src="./images/6.jpg" alt="" class="img-responsive">
+                <div class="depsition">
+                    <a href="#">Women, WOMEN</a>
+                    <a href="#"><p>Pastel Dress</p></a>
+                    <span class="price">$215.00</span>
+                </div>
+            </div>
+            <div class="col-md-5th-1 col-sm-4">
+                <img src="./images/7.jpg" alt="" class="img-responsive">
+                <div class="depsition">
+                    <a href="#">Clothes, Women</a>
+
+                    <a href="#"><p>Pastel Dress</p></a>
+                    <span class="price">$60.00 – $78.00</span>
+                </div>
+            
+            </div>
+            <div class="col-md-5th-1 col-sm-4">
+                <img src="./images/8.jpg" alt="" class="img-responsive">
+                <div class="depsition">
+                    <a href="#">Shoes, Women, WOMEN</a>
+                    <a href="#"><p>Pastel Dress</p></a>
+                    <span class="price">$280.00</span>
+                </div>
+            </div>
+            <div class="col-md-5th-1 col-sm-4">
+                <img src="./images/9.jpg" alt="" class="img-responsive">
+                <div class="depsition">
+                    <a href="#">Clothes, Dresses, Women, WOMEN</a>
+                    <a href="#"><p>Pastel Dress</p></a>
+                    <span class="price">$215.00</span>
+                </div>
+            </div>
+
+
+       
+
+        </div>
+        
+    </div>
+</div>
+</div>
---
+++ 7/js/7.js
@@ -0,0 +1,15 @@
+var swiper = new Swiper('.swiper-container', {
+    loop: true,
+    pagination: {
+        el: '.swiper-pagination',
+        dynamicBullets: true,
+    },
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+    },
+});
