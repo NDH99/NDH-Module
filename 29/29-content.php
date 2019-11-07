b9aff240a8c082fe5793d98c77822c164b353286
NDH99 <hieunguyenduca5@gmail.com>
2019-11-07 10:36:51 +0700

    commit

---
+++ 29/29-content.php
@@ -0,0 +1,63 @@
+<div class="type-29">
+    <div class="container">
+        <div class="row">
+            <div class="topbanner">
+                <div class="col-md-1 socola"></div>
+                <div class="col-md-9 socola2">
+                    <h2 class="adudu-gachchan">On sale Product</h2>
+                </div>
+                <div class="col-md-2 socola1"></div>
+            </div>
+            <div class="swiper-container">
+            <div class="swiper-wrapper">
+                <div class="swiper-slide">
+                    <figure class="card card-product">
+                        <div class="img-wrap">
+                            <img src="images/product-img-1.jpg">
+                            <div class="middle">
+                                <span class="text">SALE</span>
+                            </div>
+                        </div>
+                        <figcaption class="info-wrap">
+                            <a href="#" class="tag">Accessories</a><br>
+                            <a href="#">Chic Scarf</a><br>
+                            <span class="price">$350.00</span>
+                        </figcaption>
+                    </figure>
+                </div>
+                <div class="swiper-slide">
+                    <figure class="card card-product">
+                        <div class="img-wrap">
+                            <img src="images/product-img-11.jpg">
+                        </div>
+                        <figcaption class="info-wrap">
+                            <a href="#" class="tag">Accessories</a><br>
+                            <a href="#">Chic Scarf</a><br>
+                            <span class="price">$350.00</span>
+                        </figcaption>
+                    </figure>
+                </div>
+                <div class="swiper-slide">
+                    <figure class="card card-product">
+                        <div class="img-wrap">
+                            <img src="images/product-img-9.jpg">
+                        </div>
+                        <figcaption class="info-wrap">
+                            <a href="#" class="tag">Accessories</a><br>
+                            <a href="#">Chic Scarf</a><br>
+                            <span class="price">$350.00</span>
+                        </figcaption>
+                    </figure>
+                </div>
+            </div>
+            <!-- Add Pagination -->
+            <div class="swiper-pagination"></div>
+            <!-- Add Arrows -->
+            <br>
+            <br>
+            <div class="swiper-button-next swiper-button-black"></div>
+            <div class="swiper-button-prev swiper-button-black"></div>
+        </div>
+        </div>
+    </div>
+</div>
