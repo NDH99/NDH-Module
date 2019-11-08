5468fa70bd383b75ce8d015846956f425c030726
NDH99 <hieunguyenduca5@gmail.com>
2019-11-08 08:25:31 +0700

    commit

--- 14/14-content.php
+++ 14/14-content.php
@@ -9,15 +9,36 @@ $url_path = str_replace('\\', '/', $url_path);
 ?>
 <div class="type-14">
    <div class="container">
-       <div class="row">
-           <div class="col-md-6">
-               <img src="images/1.png" alt="responsive" class="img-responsive">
-           </div>
-           <div class="col-md-6">
-               <img src="images/2.png" alt="responsive" class="img-responsive">
-               <p>SLIDE FLAT SANDAL</p>
-               <label>Bestsellers</label>
-           </div>
-       </div>
+        <div class="row pro">
+            <div class="col-md-6">
+                <img src="images/h8-img-1.jpg" alt="responsive" class="img-responsive">
+                <a href="#" class="title"><p>Handmade Straw Bag</p></a>
+                <a href="#" class="group">Bags</a>
+            </div>
+            <div class="col-md-6">
+                <img src="images/product-img-61.jpg" alt="responsive" class="img-responsive">
+                
+            </div>
+        </div>
+        <div class="row pro">
+            <div class="col-md-6">
+                <img src="images/product-img-63.jpg" alt="responsive" class="img-responsive">
+            </div>
+            <div class="col-md-6">
+                <img src="images/h8-img-3.jpg" alt="responsive" class="img-responsive">
+                <a href="#" class="title"><p>Retro Fedora Hat</p></a>
+                <a href="#" class="group">Women - WOMEN</a>
+            </div>
+        </div>
+        <div class="row pro">
+            <div class="col-md-6">
+                <img src="images/2.png" alt="responsive" class="img-responsive">
+                <a href="#" class="title"><p>SLIDE FLAT SANDAL</p></a>
+                <a href="#" class="group">Bestsellers</a>
+            </div>
+            <div class="col-md-6">
+                <img src="images/1.png" alt="responsive" class="img-responsive">
+            </div>
+        </div>
    </div>
 </div>
