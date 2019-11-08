2466047b8c0f0ae800a66f5304f4f8afb58313ea
NDH99 <hieunguyenduca5@gmail.com>
2019-11-08 07:31:56 +0700

    Update 8-content.php

--- 8/8-content.php
+++ 8/8-content.php
@@ -1,17 +1,20 @@
 <div class="type-8">
     <div class="container">
-        <div class="row pro">
-            <div class="col-xs-12 col-sm-6 col-md-4 women">
-                <img src="images/product-category-img-1.jpg" alt="women" class="img-responsive">
-                <p>WOMEN</p>
-            </div>
-            <div class="col-xs-12 col-sm-6 col-md-4 men">
-                <img src="images/product-category-img-2.jpg" alt="men" class="img-responsive">
-                <p>MEN</p>
-            </div>
-            <div class="col-xs-12 col-sm-6 col-md-4 kid">
-                <img src="images/product-category-img-3.jpg" alt="kid" class="img-responsive">
-                <p>KID</p>
+
+        <div class="swiper-container">
+            <div class="swiper-wrapper pro">
+                <div class="swiper-slide women">
+                    <img src="images/product-category-img-1.jpg" alt="women" class="img-responsive">
+                    <p>WOMEN</p>
+                </div>
+                <div class="swiper-slide men">
+                    <img src="images/product-category-img-2.jpg" alt="men" class="img-responsive">
+                    <p>MEN</p>
+                </div>
+                <div class="swiper-slide kid">
+                    <img src="images/product-category-img-3.jpg" alt="kid" class="img-responsive">
+                    <p>KID</p>
+                </div>
             </div>
         </div>
     </div>
