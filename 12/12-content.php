7a9c634784501bcba9c9486b6857a4092c07e230
NDH99 <hieunguyenduca5@gmail.com>
2019-11-08 08:05:52 +0700

    commit

--- 12/12-content.php
+++ 12/12-content.php
@@ -6,57 +6,66 @@
         <div class="comment">
             <ul>
                 <li>
-                    <img src="images/User-2-100x100.jpg" alt="user1" class="img-responsive">
-                    <div class="image">
-                        <div class="text">
-                            <div class="name">Ed George -</div>
-                            <div class="date">November 15, 2018</div>
+                    <div class="comments">
+                        <div class="avatar">
+                            <img src="images/User-2-100x100.jpg" alt="user1" class="img-responsive">
+                        </div>
+                        <div class="comment">
+                            <div class="content">
+                                <p>Ed George - November 15, 2018</p>
+                                <p class="text-holder">Awesome and one of my favorites this season!</p>
+                            </div>
                             <div class="action">
                                 <a href="#">reply</a>
                             </div>
-                            <div class="text-holder">Awesome and one of my favorites this season!</div>
                         </div>
                     </div>
                 </li>
                 <li>
-                    
-                    <img src="images/User-3-100x100.jpg" alt="user1" class="img-responsive">
-                    <div class="image">
-                        <div class="text">
-                            <div class="name">Ed George -</div>
-                            <div class="date">November 15, 2018</div>
+                    <div class="comments">
+                        <div class="avatar">
+                            <img src="images/User-3-100x100.jpg" alt="user1" class="img-responsive">
+                        </div>
+                        <div class="comment">
+                            <div class="content">
+                                <p>Jill Patton - November 15, 2018</p>
+                                <p class="text-holder">Loving this one.</p>
+                            </div>
                             <div class="action">
                                 <a href="#">reply</a>
                             </div>
-                            <div class="text-holder">Awesome and one of my favorites this season!</div>
                         </div>
                     </div>
                 </li>
                 <li>
-
-                        <img src="images/User-4-100x100.jpg" alt="user1" class="img-responsive">
-                    <div class="image">
-                        <div class="text">
-                            <div class="name">Ed George -</div>
-                            <div class="date">November 15, 2018</div>
+                    <div class="comments">
+                        <div class="avatar">
+                            <img src="images/User-4-100x100.jpg" alt="user1" class="img-responsive">
+                        </div>
+                        <div class="comment">
+                            <div class="content">
+                                <p>Sara Brock - November 15, 2018</p>
+                                <p class="text-holder">Awesome and one of my favorite summer pieces from this amazing season!</p>
+                            </div>
                             <div class="action">
                                 <a href="#">reply</a>
                             </div>
-                            <div class="text-holder">Awesome and one of my favorites this season!</div>
                         </div>
                     </div>
                 </li>
                 <li>
-
-                        <img src="images/User-5-100x100.jpg" alt="user1" class="img-responsive">
-                    <div class="image">
-                        <div class="text">
-                            <div class="name">Ed George -</div>
-                            <div class="date">November 15, 2018</div>
+                    <div class="comments">
+                        <div class="avatar">
+                            <img src="images/User-5-100x100.jpg" alt="user1" class="img-responsive">
+                        </div>
+                        <div class="comment">
+                            <div class="content">
+                                <p>Tara Ruiz - November 15, 2018</p>
+                                <p class="text-holder">Maybe the best one so far, great job guys!</p>
+                            </div>
                             <div class="action">
                                 <a href="#">reply</a>
                             </div>
-                            <div class="text-holder">Awesome and one of my favorites this season!</div>
                         </div>
                     </div>
                 </li>
